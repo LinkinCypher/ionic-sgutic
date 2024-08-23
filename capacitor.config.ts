f533378f57c7c13acd4d3d3d5839f5039294365d
Cristhian Mora <linkincypher@hotmail.com>
2024-08-23 15:52:33 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-sgutic',
+  webDir: 'www'
+};
+
+export default config;
