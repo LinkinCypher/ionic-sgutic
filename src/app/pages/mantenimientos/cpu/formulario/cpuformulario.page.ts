f5429a74d4eaaf2f8599ba18d777248f92a61803
Cristhian Mora <linkincypher@hotmail.com>
2024-09-23 16:03:51 -0500

    añadiendo datatables

--- src/app/pages/mantenimientos/cpu/formulario/cpuformulario.page.ts
+++ src/app/pages/mantenimientos/cpu/formulario/cpuformulario.page.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { FormularioService } from 'src/app/servicios/formulario.service';
 import { Router } from '@angular/router';
+import * as $ from 'jquery'; // Importa jQuery
+import 'datatables.net'; // Importa DataTables
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './cpuformulario.page.html',
   styleUrls: ['./cpuformulario.page.scss'],
 })
-export class CpuFormularioPage implements OnInit {
+export class CpuFormularioPage implements OnInit, AfterViewInit {
   formularios: any[] = [];
 
   constructor(
@@ -24,6 +26,7 @@ export class CpuFormularioPage implements OnInit {
       (data) => {
         this.formularios = data;
         console.log('Formularios cargados:', this.formularios);
+        this.inicializarDataTable(); // Inicializa DataTables después de cargar los datos
       },
       (error) => {
         console.error('Error al obtener los formularios:', error);
@@ -31,7 +34,18 @@ export class CpuFormularioPage implements OnInit {
     );
   }
 
-  regresar() {
-    this.router.navigate(['/usuarios-admin']); // Redirige a la página de administración de usuarios
+  ngAfterViewInit() {
+    this.inicializarDataTable(); // Inicializa DataTables después de que la vista esté lista
+  }
+
+  inicializarDataTable() {
+    // Espera a que la vista y los datos se carguen completamente
+    setTimeout(() => {
+      $('#formulariosTable').DataTable();
+    }, 1000);
+  }
+
+  recargarPagina() {
+    this.cargarMisFormularios(); // Vuelve a cargar los formularios
   }
 }
