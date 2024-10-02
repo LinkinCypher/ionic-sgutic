4bbdc6523c2463aba9b3c83bc112cd2a0458e2dd
Cristhian Mora <linkincypher@hotmail.com>
2024-10-02 17:54:59 -0500

    cambio de datatable a ion-grid

--- src/app/pages/mantenimientos/cpu/admin/cpu-admin.page.ts
+++ src/app/pages/mantenimientos/cpu/admin/cpu-admin.page.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormularioService } from 'src/app/servicios/formulario.service';
-import * as $ from 'jquery'; // Importa jQuery
-import 'datatables.net'; // Importa DataTables
 
 @Component({
   selector: 'app-formulario',
@@ -10,7 +8,6 @@ import 'datatables.net'; // Importa DataTables
 })
 export class CpuFormularioPage implements OnInit {
   formularios: any[] = [];
-  dataTable: any;
 
   constructor(
     private formularioService: FormularioService
@@ -25,7 +22,6 @@ export class CpuFormularioPage implements OnInit {
       (data) => {
         this.formularios = data;
         console.log('Formularios cargados:', this.formularios);
-        this.inicializarDataTable(); // Inicializa DataTables después de cargar los datos
       },
       (error) => {
         console.error('Error al obtener los formularios:', error);
@@ -33,57 +29,8 @@ export class CpuFormularioPage implements OnInit {
     );
   }
 
-  inicializarDataTable() {
-    if (this.dataTable) {
-      this.dataTable.destroy(); // Destruye la instancia anterior si existe
-    }
-    
-    this.dataTable = $('#formulariosTable').DataTable({
-      data: this.formularios,
-      scrollX: true,
-      columns: [
-        { data: 'numeroFormulario' },
-        { data: 'institucion' },
-        { data: 'provincia' },
-        { data: 'edificio' },
-        { data: 'ubicacion' },
-        { data: 'articulo' },
-        { data: 'marca' },
-        { data: 'modelo' },
-        { data: 'serie' },
-        { data: 'activoFijo' },
-        { data: 'memoria' },
-        { data: 'procesador' },
-        { data: 'sistemaOperativo' },
-        { data: 'discoDuro' },
-        { data: 'porcentajeDiscoDuro' },
-        { data: 'observacion' }
-      ],
-      language: {
-        "decimal": "",
-        "emptyTable": "No hay información",
-        "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
-        "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
-        "infoFiltered": "(Filtrado de _MAX_ total entradas)",
-        "infoPostFix": "",
-        "thousands": ",",
-        "lengthMenu": "Mostrar _MENU_ Entradas",
-        "loadingRecords": "Cargando...",
-        "processing": "Procesando...",
-        "search": "Buscar:",
-        "zeroRecords": "Sin resultados encontrados",
-        "paginate": {
-            "first": "Primero",
-            "last": "Ultimo",
-            "next": "Siguiente",
-            "previous": "Anterior"
-        }
-      },
-    });
-  }
-
   recargarPagina() {
-    // Recargar los formularios y reiniciar la tabla
-    this.cargarMisFormularios();
+    this.cargarMisFormularios(); // Recargar los formularios
   }
+  
 }
